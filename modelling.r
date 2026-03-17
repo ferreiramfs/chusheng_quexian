300cba1bd6f5051f32dd67874d0ee45285b755dc
Mahe <santos.fd.matheus@gmail.com>
2026-03-17 14:52:29 -0300

    feat: temporal glm and results

--- modelling.r
+++ modelling.r
@@ -1,15 +1,19 @@
+#################################################################################
+#----------------------------Preparacao dos dados-------------------------------#
+#################################################################################
 library(fst)
 library(car)
 library(arm)
 library(lmtest)
-#library(statmod)
+library(statmod)
 library(ggplot2)
 library(generalhoslem)
 #library(ResourceSelection)
 
-
 data <- read_fst("data/dados_comprimidos.fst")
 summary(data)
+
+data$ANO_NASC <- format(data$DTNASC, "%Y")
 
 variaveis_explicativas <- c('LOCNASC', 'IDADEMAE', 'ESTCIVMAE', 'ESCMAE', 'QTDGESTANT', 'GRAVIDEZ',
                             'PARTO', 'CONSULTAS', 'SEXO', 'APGAR1', 'APGAR5', 'RACACORMAE', 'PESO', 'SEMAGESTAC')
@@ -21,6 +25,11 @@ anomalias <- c('Defeito do Tubo Neural', 'Microcefalia', 'Cardiopatias Congênit
                'Defeitos de Membros', 'Defeitos de Parede Abdominal', 'Síndrome de Down')
 
 vars <- c(variaveis_explicativas, indicadores)
+
+
+#################################################################################
+#---------------------Modelo Inicial com todas variaveis------------------------#
+#################################################################################
 
 ajuste_logito <- glm(`Defeito do Tubo Neural` ~ LOCNASC + IDADEMAE + ESTCIVMAE + 
                        ESCMAE + QTDGESTANT + GRAVIDEZ + PARTO + CONSULTAS + 
@@ -63,9 +72,11 @@ ggplot(data = data_calibra, aes(x = Esperado, y = Observado))+
   theme_bw(base_size = 14)+
   geom_abline(intercept = 0, slope = 1, col = 'red', linewidth = 1)
 
-#Removendo o IDHM devido a multicolinearidade
+#################################################################################
+#-----------------Removendo o IDHM devido a multicolinearidade------------------#
+#################################################################################
 ajuste_logito2 <- update(ajuste_logito, ~ . - idhm - renda_domiciliar_per_capita)
-rm(ajuste_logito)
+#rm(ajuste_logito)
 
 summary(ajuste_logito2)
 
@@ -76,7 +87,7 @@ vif(ajuste_logito2)
 residuos <- qresiduals(ajuste_logito)
 qqnorm(residuos)
 qqline(residuos)
-plot(log1p(fitted(ajuste_logito2)), residuos)
+plot(fitted(ajuste_logito2), residuos)
 
 #Teste de Hosmer-Lemeshow (Qualidade do Ajuste)
 obs_modelo <- ajuste_logito2$y
@@ -98,3 +109,48 @@ ggplot(data = data_calibra, aes(x = Esperado, y = Observado))+
 
 #Pontos Influentes/Outliers
 influenceIndexPlot(ajuste_logito2, vars = c('Studentized','Cook','Hat'), id.n = 3, cex = 1.4)
+
+#################################################################################
+#----------------------Modelo inserindo a variavel ano--------------------------#
+#################################################################################
+
+ajuste_log_temp <- glm(`Defeito do Tubo Neural` ~ LOCNASC + IDADEMAE + ESTCIVMAE + 
+                       ESCMAE + QTDGESTANT + GRAVIDEZ + PARTO + CONSULTAS + 
+                       SEXO + APGAR1 + APGAR5 + RACACORMAE + PESO + SEMAGESTAC + 
+                       idhm_educacao + idhm_longevidade + idhm_renda + 
+                       porcentagem_da_populacao_baixa_renda + cobertura_bcg + mortalidade + 
+                       + taxa_de_analfabetismo + ANO_NASC,
+                     family = binomial(link = 'logit'),
+                     data = data)
+
+summary(ajuste_log_temp)
+
+#Multicolinearidade
+vif(ajuste_log_temp)
+
+#Resíduos quantílicos normalizados aleatorizados:
+residuos <- qresiduals(ajuste_log_temp)
+qqnorm(residuos)
+qqline(residuos)
+plot(fitted(ajuste_log_temp), residuos)
+
+#Teste de Hosmer-Lemeshow (Qualidade do Ajuste)
+obs_modelo <- ajuste_log_temp$y
+pred_modelo <- fitted(ajuste_log_temp)
+
+CH_test <- logitgof(obs_modelo, pred_modelo, g = 10)
+print(CH_test)
+
+#Curva de calibração
+CH_test_obs <- CH_test$observed[,2]
+CH_test_exp <- CH_test$expected[,2]
+
+data_calibra <- data.frame(Esperado = CH_test_exp, Observado = CH_test_obs)
+
+ggplot(data = data_calibra, aes(x = Esperado, y = Observado))+
+  geom_point(size = 2)+
+  theme_bw(base_size = 14)+
+  geom_abline(intercept = 0, slope = 1, col = 'red', linewidth = 1)
+
+#Pontos Influentes/Outliers
+influenceIndexPlot(ajuste_log_temp, vars = c('Studentized','Cook','Hat'), id.n = 3, cex = 1.4)
